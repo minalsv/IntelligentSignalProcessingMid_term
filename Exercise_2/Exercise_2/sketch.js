57a4dd25e9ed5911bb07abced9365ead0fb2c4b0
Minal Vaishampayan <minalsv@gmail.com>
2023-07-13 21:26:11 +0200

    Move spectrum and arc drawing to functions and use feature colors

--- Exercise_2/Exercise_2/sketch.js
+++ Exercise_2/Exercise_2/sketch.js
@@ -1,97 +1,144 @@
-var mySound;
-var playStopButton;
-var jumpButton;
-var sliderVolume;
-var sliderRate;
-var sliderPan;
+/*Ref: basic code is taken from the fft analyzer example exercise_8 from the course and modified it*/
+let mySound;
+let analyzer;
+let features;
+let startAnalyzer;
+let analyzerStarted;
 
-var fft;
+let file1Features = ['rms', 'zcr', 'spectralCrest', 'energy'];
+let lowerRight;
+let lowerLeft;
+let upperLeft;
+let upperRight;
+
+const featureColors = {
+    rms: [0, 0, 205, 200],
+    spectralCentroid: [65, 105, 225, 200],
+    mfcc: [255, 69, 0, 200],
+    zcr: [255, 165, 0, 200],
+    spectralCrest: [0, 206, 209, 200],
+    energy: [139, 0, 0, 200]
+};
 
 function preload() {
-  soundFormats('wav', 'mp3');
-  mySound = loadSound('/sounds/233709__x86cam__130bpm-32-beat-loop_v2');
+    soundFormats('mp3', 'ogg', 'wav');
+    mySound = loadSound('../sounds/Ex2_sound2.wav');
+
+
+    startAnalyzer = false;
+    analyzerStarted = false;
 }
 
 function setup() {
-  createCanvas(400, 400);
-  background(180);
-    
-  playStopButton = createButton('play');
-  playStopButton.position(200, 20);
-  playStopButton.mousePressed(playStopSound);
-  jumpButton = createButton('jump');
-  jumpButton.position(250, 20);
-  jumpButton.mousePressed(jumpSong);
+    createCanvas(400, 400);
+    background(180);
 
-  sliderVolume = createSlider(0, 1, 1, 0.01);
-  sliderVolume.position(20,25);
-  sliderRate = createSlider(-2, 2, 1, 0.01);
-  sliderRate.position(20,70);
-  sliderPan = createSlider(-1, 1, 0, 0.01);
-  sliderPan.position(20,115);
-  
-  fft = new p5.FFT(0.2, 2048);
-}
+    playStopButton = createButton('play');
+    playStopButton.position(200, 20);
+    playStopButton.mousePressed(playStopSound);
+    jumpButton = createButton('jump');
+    jumpButton.position(250, 20);
+    jumpButton.mousePressed(jumpSong);
+
+    sliderVolume = createSlider(0, 1, 1, 0.01);
+    sliderVolume.position(20, 25);
+    sliderRate = createSlider(-2, 2, 1, 0.01);
+    sliderRate.position(20, 70);
+    sliderPan = createSlider(-1, 1, 0, 0.01);
+    sliderPan.position(20, 115);
+
+    fft = new p5.FFT(0.2, 2048);
+
+    if (typeof Meyda == 'undefined') {
+        cpnsole.log("Error: No Meyda libraries found");
+    } else {
+        console.log("Meyda is present!");
+        // Create an AudioContext
+        const audioContext = getAudioContext();
+        analyzer =
+            analyzer = Meyda.createMeydaAnalyzer({
+                audioContext: audioContext,
+                source: mySound,
+                bufferSize: 512, // Adjust buffer size as needed
+                featureExtractors: file1Features, // Add desired features
+                callback: features => {
+                    console.log(features);
+                    lowerRight = features.rms * 1000; //0.022
+                    lowerLeft = features.zcr * 10; //10
+                    upperLeft = features.spectralCrest * 10; //7.9
+                    upperRight = features.energy * 10; //0.5
+                    console.log(lowerRight, lowerLeft, upperLeft, upperRight);
+                } // Function to handle the extracted features
+            });
+        console.log("Analyzer initialized!");
+
+    }
+
+};
 
 function draw() {
-  background(180, 100);
+    background(180, 100);
 
-  fill(0);
-  text('volume', 80,20);
-  text('rate', 80,65);
-  text('pan', 80,110);  
-  
-  let vol = Math.pow(sliderVolume.value(), 3);                  
-  mySound.setVolume(vol);
-  mySound.rate(sliderRate.value());
-  mySound.pan(sliderPan.value());
+    fill(0);
+    text('volume', 80, 20);
+    text('rate', 80, 65);
+    text('pan', 80, 110);
+
+    let vol = Math.pow(sliderVolume.value(), 3);
+    mySound.setVolume(vol);
+    mySound.rate(sliderRate.value());
+    mySound.pan(sliderPan.value());
+
+    let spectrum = fft.analyze();
+    drawSpectrum(spectrum);
+    drawArcs();
+}
+
+function drawSpectrum(spectrum) {
     
-  let spectrum = fft.analyze();
-  
-  push();
-  translate(200,50);
-  scale(0.33, 0.20);
-  noStroke();
-  fill(60);
-  rect(0, 0, width, height);
-  fill(255, 0, 0);
-  for (let i = 0; i< spectrum.length; i++){
-    let x = map(i, 0, spectrum.length, 0, width);
-    let h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, width/spectrum.length, h);
-  }
-  pop();
-    
-  fill(30, 30, 255, 200);
-  let treble = fft.getEnergy("treble");
-  let lowMid = fft.getEnergy("lowMid");
-  let mid = fft.getEnergy("mid");
-  let highMid = fft.getEnergy("highMid");
-  arc(200, 275, treble, treble, 0, HALF_PI);
-  fill(100, 55, 255, 200);
-  arc(200, 275, lowMid, lowMid, HALF_PI, PI);
-  fill(55, 100, 255, 200);
-  arc(200, 275, mid, mid, PI, PI+HALF_PI);
-  fill(130, 130, 255, 200);
-  arc(200, 275, highMid, highMid, PI+HALF_PI, 2*PI);
+    push();
+    translate(200, 50);
+    scale(0.33, 0.20);
+    noStroke();
+    fill(60);
+    rect(0, 0, width, height);
+    fill(255, 0, 0);
+    for (let i = 0; i < spectrum.length; i++) {
+        let x = map(i, 0, spectrum.length, 0, width);
+        let h = -height + map(spectrum[i], 0, 255, height, 0);
+        rect(x, height, width / spectrum.length, h);
+    }
+    pop();
+}
+
+function drawArcs() {
+    fill(featureColors[file1Features[0]]);
+    arc(200, 275, lowerLeft, lowerLeft, 0, HALF_PI);
+    fill(featureColors[file1Features[1]]);
+    arc(200, 275, lowerRight, lowerRight, HALF_PI, PI);
+    fill(featureColors[file1Features[2]]);
+    arc(200, 275, upperRight, upperRight, PI, PI + HALF_PI);
+    fill(featureColors[file1Features[3]]);
+    arc(200, 275, upperLeft, upperLeft, PI + HALF_PI, 2 * PI);
 }
 
 function jumpSong() {
-  var dur = mySound.duration();
-  var t = random(dur);
-  mySound.jump(t);
+    var dur = mySound.duration();
+    var t = random(dur);
+    mySound.jump(t);
 }
 
 function playStopSound() {
-  if (mySound.isPlaying())
-    {
-      mySound.stop();
-      //mySound.pause();
-      playStopButton.html('play');
-      background(180);
+    if (mySound.isPlaying()) {
+        mySound.stop();
+        analyzer.stop();
+        //mySound.pause();
+        playStopButton.html('play');
+        background(180);
     } else {
-      //mySound.play();
-      mySound.loop()            
-      playStopButton.html('stop');
-    }  
+        //mySound.play();
+        mySound.loop()
+        analyzer.start();
+        playStopButton.html('stop');
+    }
 }
