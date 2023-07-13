ef8fdeccd99db1167a6248d0f0811bb00760dbfa
Minal Vaishampayan <minalsv@gmail.com>
2023-07-13 22:14:59 +0200

    Add file settings to select the file to be tested

--- Exercise_2/Exercise_2/sketch.js
+++ Exercise_2/Exercise_2/sketch.js
@@ -2,14 +2,29 @@
 let mySound;
 let analyzer;
 let features;
-let startAnalyzer;
-let analyzerStarted;
 
-let file1Features = ['rms', 'zcr', 'spectralCrest', 'energy'];
+
+let file1 = '../sounds/Ex2_sound1.wav';
+let file1Features = ['rms', 'zcr', 'spectralCrest', 'spectralKurtosis'];
+let file1Factors = [1000,10,10,10];
+
+let file2 = '../sounds/Ex2_sound2.wav';
+let file2Features = ['rms', 'zcr', 'spectralCrest', 'energy'];
+let file2Factors = [1000,10,10,100];
+
+let file3 = '../sounds/Ex2_sound3.wav';
+let file3Features = ['rms', 'zcr', 'spectralCrest', 'energy'];
+let file3Factors = [1000,10,10,100];
+
+
 let lowerRight;
 let lowerLeft;
 let upperLeft;
 let upperRight;
+
+let currentFeatures = file2Features;
+let currentFile = file2;
+let currentFactors = file2Factors;
 
 const featureColors = {
     rms: [0, 0, 205, 200],
@@ -17,16 +32,13 @@ const featureColors = {
     mfcc: [255, 69, 0, 200],
     zcr: [255, 165, 0, 200],
     spectralCrest: [0, 206, 209, 200],
-    energy: [139, 0, 0, 200]
+    energy: [139, 0, 0, 200],
+    spectralKurtosis:[138, 43, 226,200]
 };
 
 function preload() {
     soundFormats('mp3', 'ogg', 'wav');
-    mySound = loadSound('../sounds/Ex2_sound2.wav');
-
-
-    startAnalyzer = false;
-    analyzerStarted = false;
+    mySound = loadSound(currentFile);
 }
 
 function setup() {
@@ -39,6 +51,7 @@ function setup() {
     jumpButton = createButton('jump');
     jumpButton.position(250, 20);
     jumpButton.mousePressed(jumpSong);
+
 
     sliderVolume = createSlider(0, 1, 1, 0.01);
     sliderVolume.position(20, 25);
@@ -60,13 +73,13 @@ function setup() {
                 audioContext: audioContext,
                 source: mySound,
                 bufferSize: 512, // Adjust buffer size as needed
-                featureExtractors: file1Features, // Add desired features
+                featureExtractors: currentFeatures, // Add desired features
                 callback: features => {
                     console.log(features);
-                    lowerRight = features.rms * 1000; //0.022
-                    lowerLeft = features.zcr * 10; //10
-                    upperLeft = features.spectralCrest * 10; //7.9
-                    upperRight = features.energy * 10; //0.5
+                    lowerRight = features[currentFeatures[0]] * currentFactors[0]; 
+                    lowerLeft = features[currentFeatures[1]] * currentFactors[1]; 
+                    upperLeft = features[currentFeatures[2]] * currentFactors[2]; 
+                    upperRight = features[currentFeatures[3]] * currentFactors[3]; 
                     console.log(lowerRight, lowerLeft, upperLeft, upperRight);
                 } // Function to handle the extracted features
             });
@@ -90,14 +103,14 @@ function draw() {
     mySound.pan(sliderPan.value());
 
     let spectrum = fft.analyze();
-    drawSpectrum(spectrum);
+    drawSpectrum(spectrum,200,50);
     drawArcs();
 }
 
-function drawSpectrum(spectrum) {
+function drawSpectrum(spectrum,translateX,translatey) {
     
     push();
-    translate(200, 50);
+    translate(translateX, translatey);
     scale(0.33, 0.20);
     noStroke();
     fill(60);
@@ -112,13 +125,13 @@ function drawSpectrum(spectrum) {
 }
 
 function drawArcs() {
-    fill(featureColors[file1Features[0]]);
+    fill(featureColors[currentFeatures[0]]);
     arc(200, 275, lowerLeft, lowerLeft, 0, HALF_PI);
-    fill(featureColors[file1Features[1]]);
+    fill(featureColors[currentFeatures[1]]);
     arc(200, 275, lowerRight, lowerRight, HALF_PI, PI);
-    fill(featureColors[file1Features[2]]);
+    fill(featureColors[currentFeatures[2]]);
     arc(200, 275, upperRight, upperRight, PI, PI + HALF_PI);
-    fill(featureColors[file1Features[3]]);
+    fill(featureColors[currentFeatures[3]]);
     arc(200, 275, upperLeft, upperLeft, PI + HALF_PI, 2 * PI);
 }
 
